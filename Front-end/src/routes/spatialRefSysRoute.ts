dc5f90d6028322940478d0477f91ea89e3834ebf
Fillipe Almeida <120722022+FPbueno@users.noreply.github.com>
2024-04-12 18:57:10 -0300

    Delete Front-end directory
    foi adicionado o arquivo errado, motivo qual estou deletando-o

--- Front-end/src/routes/spatialRefSysRoute.ts
+++
@@ -1,9 +0,0 @@
-import SpatialRefSysController from '../controller/SpatialRefSysController';
-
-var express = require('express')
-const router = express.Router();
-
-router.get('/spatial_ref_sys', SpatialRefSysController.getSpatialRefSys);
-
-
-export default router;
