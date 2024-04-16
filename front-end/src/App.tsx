595ba6db3bd18bb5af87bdcbe9ef594468f927d7
Fillipe Almeida <120722022+FPbueno@users.noreply.github.com>
2024-04-16 19:57:24 -0300

    adicionado interface para tela de login

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,3 +1,4 @@
+import Login from "./components/Login";
 import Alteracao from "./pages/Alteracao";
 import Aoi from "./pages/Aoi";
 import Apontamento from "./pages/Apontamento";
@@ -6,16 +7,7 @@ import Grade from "./pages/Grade";
 function App() {
 
   return (
-    <div>
-      <p>AOI</p>
-      <Aoi />
-      <p>Apontamentos</p>
-      <Apontamento />
-      <p>Grade</p>
-      <Grade />
-      <p>Alteração</p>
-      <Alteracao />
-    </div>
+    <Login />
   )
 }
 
