902064f4f9e7f66b9562fac34b7d96872bf8022e
Isaac - Exon <isaac.souza@exongenomics.com>
2024-04-18 12:40:48 -0300

    Ajuste css

--- front-end/src/components/HeaderComponent.tsx
+++ front-end/src/components/HeaderComponent.tsx
@@ -1,26 +1,28 @@
 interface Button {
-    name: string;
-    onClick: ()=>void;
+  name: string;
+  onClick: () => void;
 }
 
 interface HeaderProps {
-    buttons: Button[];
+  buttons: Button[];
 }
 
-
-const HeaderComponent = ({buttons} : HeaderProps) =>{
-
-    return(
-        <div className="h-20 flex items-center justify-center bg-gradient-to-r from-orange-500 to-orange-700 text-white text-center text-4xl font-bold shadow-md">
-            {buttons.map((button, index) =>(
-                <button
-                    className="p-4 bg-blue-500 text-white hover:bg-blue-800 m-2 rounded-xl"
-                    key={index} 
-                    onClick={()=> {button.onClick()}}
-                >{button.name}</button>
-            ))}
-        </div>
-    )
-}
+const HeaderComponent = ({ buttons }: HeaderProps) => {
+  return (
+    <div className="h-20 flex items-center justify-center bg-gradient-to-r from-orange-500 to-orange-700 text-white text-center text-4xl font-bold shadow-md">
+      {buttons.map((button, index) => (
+        <button
+          className="p-4  text-white hover:bg-orange-800 m-2 rounded-xl"
+          key={index}
+          onClick={() => {
+            button.onClick();
+          }}
+        >
+          {button.name}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 export default HeaderComponent;
