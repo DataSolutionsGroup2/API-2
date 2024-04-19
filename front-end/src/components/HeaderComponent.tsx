9278e69f56b862fd96519d22594dc41b34973f52
Isaac - Exon <isaac.souza@exongenomics.com>
2024-04-19 17:12:13 -0300

    graphic

--- front-end/src/components/HeaderComponent.tsx
+++ front-end/src/components/HeaderComponent.tsx
@@ -9,7 +9,10 @@ interface HeaderProps {
 
 const HeaderComponent = ({ buttons }: HeaderProps) => {
   return (
-    <div className="h-20 flex items-center justify-center bg-gradient-to-r from-orange-500 to-orange-700 text-white text-center text-4xl font-bold shadow-md">
+    <div className="h-20 flex items-center justify-around bg-gradient-to-r from-orange-500 to-orange-700 text-white text-center text-4xl font-bold shadow-md">
+      <ul>
+        <li>Gestor</li>
+      </ul>
       {buttons.map((button, index) => (
         <button
           className="p-4  text-white hover:bg-orange-800 m-2 rounded-xl"
