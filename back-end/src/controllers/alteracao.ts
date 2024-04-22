1415ddccace43fd42ad32107303ee1b0d18ba4b1
Rodrigo Augusto Silva Lima <rodrigoaslima@hotmail.com>
2024-04-22 18:22:15 -0300

    configuracao do banco na nuvem

--- back-end/src/controllers/alteracao.ts
+++ back-end/src/controllers/alteracao.ts
@@ -1,8 +1,12 @@
 import { Request,Response } from "express";
 import pool from "./db";
 
-export async function list(req:Request, res:Response){
-    const {tablename} = req.body;
-    const response = await pool.query(`select id,area_km2,municipio, cod_estado, cod_class, class, obs from ${tablename}`);
-    res.json(response.rows);
-}
+export async function list(req: Request, res: Response, tablename: string) {
+    try {
+      const response = await pool.query(`SELECT id, area_km2, municipio, cod_estado, cod_class, class, obs FROM ${tablename}`);
+      res.json(response.rows);
+    } catch (error) {
+      console.error("Erro ao listar dados:", error);
+      res.status(500).json({ error: "Erro ao listar dados" });
+    }
+  }
--- back-end/src/routes/alteracao.ts
+++ back-end/src/routes/alteracao.ts
@@ -3,6 +3,15 @@ import { list } from "../controllers/alteracao";
 
 const router = Router();
 
-router.post("/", list);
-
-export default router;
+router.get("/:tablename", async (req, res) => {
+    try {
+      const { tablename } = req.params; // Obter o nome da tabela dos parâmetros da URL
+      const result = await list(req, res, tablename); // Passar o nome da tabela como parâmetro
+      res.json(result);
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+      res.status(500).json({ error: "Erro ao buscar dados" });
+    }
+  });
+  
+  export default router;
