da363be59271e68fe6c1dbe9083f3ff11cb56bb0
Tiago Santini <tiagosantinidasilva@gmail.com>
2024-05-03 15:11:53 -0300

    [back end] atualizar dados de usuario(put)

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import GradeAtuacaoController from '../controllers/GradeAtuacaoController';
+import u1 from "../controllers/PutUsuarios"
 
 const router = express.Router();
 const gradeAtuacaoController = new GradeAtuacaoController();
 
 router.get('/grade-atuacao', gradeAtuacaoController.getGradeAtuacao);
+router.put('/usuarios',u1);
 
 export default router;
