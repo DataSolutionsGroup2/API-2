d1def7d4e52f3c785c74e0256fc51918368d02c3
Isaac - Exon <isaac.souza@exongenomics.com>
2024-05-05 20:03:28 -0300

    DeleteUser

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,9 +1,12 @@
-import express from 'express';
-import GradeAtuacaoController from '../controllers/GradeAtuacaoController';
+import express from "express";
+import GradeAtuacaoController from "../controllers/GradeAtuacaoController";
+import DeleteUsuario from "../controllers/DeleteUserController";
 
 const router = express.Router();
 const gradeAtuacaoController = new GradeAtuacaoController();
+const deleteUserController = new DeleteUsuario();
 
-router.get('/grade-atuacao', gradeAtuacaoController.getGradeAtuacao);
+router.get("/grade-atuacao", gradeAtuacaoController.getGradeAtuacao);
+router.delete("/usuarios", deleteUserController.deleteUsuario);
 
 export default router;
