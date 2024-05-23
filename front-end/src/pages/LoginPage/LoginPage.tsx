14df14cd93421f0abc750af753d76e982a7712a1
Fillipe Almeida <120722022+FPbueno@users.noreply.github.com>
2024-05-22 22:26:10 -0300

    Adicionado funcinalidade da tecla enter ao logar

--- front-end/src/pages/LoginPage/LoginPage.tsx
+++ front-end/src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import React, { useRef, useEffect } from 'react';
 
 
 interface UserInterface {
@@ -17,6 +18,21 @@ export default function Login() {
         password: "1234"
     }
 
+        const buttonRef = useRef<HTMLButtonElement>(null);
+
+        const handleKeyPress = (event: any) => {
+            if (event.key === 'Enter' && buttonRef.current) {
+                buttonRef.current.click();
+            }
+        };
+        useEffect(() => {
+            document.addEventListener('keydown', handleKeyPress);
+
+            return () => {
+                document.removeEventListener('keydown', handleKeyPress);
+            };
+        }, []);
+
 
     const handleUserChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setUsuario(event.target.value);
@@ -26,16 +42,16 @@ export default function Login() {
         setSenha(event.target.value);
     };
 
-    const handleLogin = ()=>{
-        const data ={
+    const handleLogin = () => {
+        const data = {
             usuario,
             senha
         }
 
-        if(mockedUser.name === usuario && mockedUser.password === senha){
+        if (mockedUser.name === usuario && mockedUser.password === senha) {
             navigate("/gestorPage");
-        } else{
-            console.log('Erro ao Logar')
+        } else {
+            alert('Erro ao Logar');
         }
         console.log('data: ', data)
     }
@@ -50,37 +66,31 @@ export default function Login() {
                         <div className="bg-white">Entre com a sua conta</div>
                         <label htmlFor="i1" className="pt-3 bg-white pr-56 font-medium">Usuario</label>
                         <input className="rounded-2xl bg-white p-1 w-72 border-solid
-                            border-visiona border-2 hover:p-1.5 hover:w-80 transition-all" 
-                            type="text" id="i1" placeholder="Entre com o seu usuário" 
+                            border-visiona border-2 hover:p-1.5 hover:w-80 transition-all"
+                            type="text" id="i1" placeholder="Entre com o seu usuário"
                             value={usuario} onChange={handleUserChange}
                         />
                         <label htmlFor="i2" className="pt-7 bg-white pr-60 font-medium">Senha</label>
                         <input className="rounded-2xl bg-white p-1 w-72 border-solid 
-                            border-visiona border-2 hover:p-1.5 hover:w-80 transition-all" 
+                            border-visiona border-2 hover:p-1.5 hover:w-80 transition-all"
                             type="password" id="i2" placeholder="Entre com o sua senha"
-                            value={senha} onChange={handlePasswordChange} 
+                            value={senha} onChange={handlePasswordChange}
                         />
                         <div className="flex flex-row mt-4">
                             <input type="checkbox" id="c1" />
                             <label htmlFor="c1" className="text-xs pl-1 text-black">Lembrar de mim.</label>
-                            <div>
-                                <p className="text-xs pl-20 text-visiona opacity-75"><a href="">Esqueceu a senha?</a></p>
-                            </div>
                         </div>
                         <div>
                             <button className="border-solid border-visiona border-2 rounded-lg
                                 bg-visiona w-32 p-1 mt-4 hover:bg-white hover:text-visiona 
                                 hover:p-1.5 hover:w-36 transition-all text-white text-bold "
-                                onClick={handleLogin}
+                                onClick={handleLogin} ref={buttonRef} id="Login"
                             >Login</button>
-                        </div>
-                        <div className="flex flex-row text-sm mt-4">
-                            <p className="text-black">Não tem conta?<a className=" text-visiona opacity-75" href=""> Criar</a></p>
                         </div>
                     </div>
                 </div>
                 <div id="direita" className="flex flex-col gap-4 justify-center items-center bg-visionai2 h-96 w-80 rounded-r-3xl shadow-2xl shadow-visiona ">
-                    <div id="texto"className="flex flex-col justify-center items-center gap-4">
+                    <div id="texto" className="flex flex-col justify-center items-center gap-4">
                     </div>
                 </div>
             </div>
@@ -91,4 +101,4 @@ export default function Login() {
 {/* <h3 className="flex flex-col justify-center items-center text-white font-semibold pl-2">
 Bem vindo de Volta!
 </h3> */}
-{/* <p className="flex flex-col justify-center items-center text-white pl-4 text-semibold">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, enim non aperiam quod cum ex similique vero aut numquam voluptate praesentium unde, asperiores amet. Obcaecati similique ab corrupti doloremque ex?</p> */}
+{/* <p className="flex flex-col justify-center items-center text-white pl-4 text-semibold">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, enim non aperiam quod cum ex similique vero aut numquam voluptate praesentium unde, asperiores amet. Obcaecati similique ab corrupti doloremque ex?</p> */ }
