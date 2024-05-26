8a8030640e738ffed56028b72796c0eb73475f5c
Isaac - Exon <isaac.souza@exongenomics.com>
2024-05-26 20:14:38 -0300

    ok

--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -1,19 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/LoginPage/LoginPage";
 import GestorPage from "./components/manager/GestorPage";
-import LayoutPage from "./pages/RelatorioPage/LayoutPage";
+
 import PageGestor from "./pages/PaginaGestor";
 
-import CriacaoEdidorRevisorModal from "./pages/Criar Usuario/CriacaoEditorRevisorModal";
-import PesquisaDataGrid from "./pages/Criar Usuario/Pesquisas/pesquisa";
+import CriacaoEdidorRevisorModal from "./pages/Gestor/Criar Usuario/CriacaoEditorRevisorModal";
+import PesquisaDataGrid from "./pages/Gestor/Pesquisas/pesquisa";
 
 function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/gestorPage" element={<GestorPage />} />
-      <Route path="/relatorios" element={<LayoutPage />} />
-      <Route path="/relatorioPage" element={<LayoutPage />} />
       <Route path="/createUser" element={<CriacaoEdidorRevisorModal />} />
       <Route path="/pagegestor" element={<PageGestor />} />
       <Route path="/pesquisa" element={<PesquisaDataGrid />} />
