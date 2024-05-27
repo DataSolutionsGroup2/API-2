32a26b1908cce45b8fe0ed09039b196d26b4ef23
Isaac-Exon <160609322+Isaac-Exon@users.noreply.github.com>
2024-05-27 12:55:13 -0300

    Add files via upload

---
+++ utils/mockedData/regionMocked.ts
@@ -0,0 +1,211 @@
+export const MockedRegion = {
+    regiao:[
+      {
+        name: "Atibaia",
+        data:[
+          {
+            id:"1",
+            atribuicao:"analista 2",
+            status: "finalizado",
+            validacao: "analista 4",
+            status_val:"finalizado",
+            area_km2: 0.28
+          },
+          {
+            id:"2",
+            atribuicao:"analista 4",
+            status: "finalizado",
+            validacao: "analista 6",
+            status_val:"andamento",
+            area_km2: 0.276289087
+          },
+          {
+            id:"3",
+            atribuicao:"analista 2",
+            status: "andamento",
+            validacao: "null",
+            status_val:"null",
+            area_km2: 0
+          },
+          {
+            id:"4",
+            atribuicao:"analista 2",
+            status: "finalizado",
+            validacao: "analista 4",
+            status_val:"andamento",
+            area_km2: 0.9876
+          },
+          {
+            id:"5",
+            atribuicao:"analista 3",
+            status: "finalizado",
+            validacao: "analista 6",
+            status_val:"finalizado",
+            area_km2: 0.28
+          },
+          {
+            id:"6",
+            atribuicao:"analista 2",
+            status: "finalizado",
+            validacao: "analista 4",
+            status_val:"finalizado",
+            area_km2: 0.28
+          },
+          {
+            id:"7",
+            atribuicao:"analista 2",
+            status: "andamento",
+            validacao: "analista 3",
+            status_val:"andamento",
+            area_km2: 0.2832123
+          },
+          {
+            id:"8",
+            atribuicao:"analista 1",
+            status: "finalizado",
+            validacao: "analista 8",
+            status_val:"finalizado",
+            area_km2: 0.763
+          },
+        ]
+      },
+      {
+        name: "Cruzeiro",
+        data:[
+          {
+            id:"1",
+            atribuicao:"analista 2",
+            status: "finalizado",
+            validacao: "analista 4",
+            status_val:"finalizado",
+            area_km2: 0.2845
+          },
+          {
+            id:"2",
+            atribuicao:"analista 4",
+            status: "finalizado",
+            validacao: "analista 6",
+            status_val:"andamento",
+            area_km2: 0.29087
+          },
+          {
+            id:"3",
+            atribuicao:"analista 2",
+            status: "andamento",
+            validacao: "null",
+            status_val:"null",
+            area_km2: 0.644
+          },
+          {
+            id:"4",
+            atribuicao:"analista 2",
+            status: "finalizado",
+            validacao: "analista 4",
+            status_val:"andamento",
+            area_km2: 0.98
+          },
+          {
+            id:"5",
+            atribuicao:"analista 3",
+            status: "finalizado",
+            validacao: "analista 6",
+            status_val:"finalizado",
+            area_km2: 0.28345
+          },
+          {
+            id:"6",
+            atribuicao:"analista 2",
+            status: "finalizado",
+            validacao: "analista 4",
+            status_val:"finalizado",
+            area_km2: 0.2346
+          },
+          {
+            id:"7",
+            atribuicao:"analista 2",
+            status: "andamento",
+            validacao: "analista 3",
+            status_val:"andamento",
+            area_km2: 0.89900
+          },
+          {
+            id:"8",
+            atribuicao:"analista 1",
+            status: "finalizado",
+            validacao: "analista 8",
+            status_val:"finalizado",
+            area_km2: 0.2677
+          },
+        ]
+      },
+      {
+        name: "Taubaté",
+        data:[
+          {
+            id:"1",
+            atribuicao:"analista 2",
+            status: "finalizado",
+            validacao: "analista 4",
+            status_val:"finalizado",
+            area_km2: 0.234566
+          },
+          {
+            id:"2",
+            atribuicao:"analista 4",
+            status: "finalizado",
+            validacao: "analista 6",
+            status_val:"andamento",
+            area_km2: 0.75437
+          },
+          {
+            id:"3",
+            atribuicao:"analista 2",
+            status: "andamento",
+            validacao: "null",
+            status_val:"null",
+            area_km2: 0.4555
+          },
+          {
+            id:"4",
+            atribuicao:"analista 2",
+            status: "finalizado",
+            validacao: "analista 4",
+            status_val:"andamento",
+            area_km2: 0.9876
+          },
+          {
+            id:"5",
+            atribuicao:"analista 3",
+            status: "finalizado",
+            validacao: "analista 6",
+            status_val:"finalizado",
+            area_km2: 0.28
+          },
+          {
+            id:"6",
+            atribuicao:"analista 2",
+            status: "finalizado",
+            validacao: "analista 4",
+            status_val:"finalizado",
+            area_km2: 0.28
+          },
+          {
+            id:"7",
+            atribuicao:"analista 2",
+            status: "andamento",
+            validacao: "analista 3",
+            status_val:"andamento",
+            area_km2: 0.2832123
+          },
+          {
+            id:"8",
+            atribuicao:"analista 1",
+            status: "finalizado",
+            validacao: "analista 8",
+            status_val:"finalizado",
+            area_km2: 0.763
+          },
+        ]
+      }  
+    ]
+  }
