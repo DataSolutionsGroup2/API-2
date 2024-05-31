dc1dc6d4a9a6d5ff076ec44e9ff244e4686ae556
Isaac - Exon <isaac.souza@exongenomics.com>
2024-05-31 09:41:03 -0300

    ajustes

--- src/components/Gestor/HeaderComponent.tsx
+++ src/components/Gestor/HeaderComponent.tsx
@@ -10,6 +10,7 @@ interface HeaderProps {
 const HeaderComponent = ({ buttons }: HeaderProps) => {
   return (
     <div className=" flex-col h-auto w-[250px] bg-blue-600 rounded-br-lg">
+      <img src="/src/imagens/logotipo_empresa_p.jpg" alt="" />
       <div className="p-4">
         <ul>
           {buttons.map((button, index) => (
