39fbce40ff94b7e3e5c6b4358971627d4203e62d
Isaac - Exon <isaac.souza@exongenomics.com>
2024-06-06 16:31:27 -0300

    ajustes de pastas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,14 +3,16 @@ import DeleteUsuarios from "../controllers/DeleteUserController";
 import u1 from "../controllers/PutUsuarios";
 import InsertUsuario from "../controllers/InsertUser";
 import GetGradeAtuacao from "../controllers/GetTbGradeAtuacaoCidades";
-import InsertOrUpdateAtribuicao from "../controllers/InserAtribuicaoTbGrade";
+import InsertOrUpdateAtribuicao from "../controllers/InserAtribuicaoTbGradeEditor";
 import GetStatistcs from "../controllers/GetStastistcs";
+import InsertOrUpdateValidacao from "../controllers/InsertValidacaoTbgradeRevisor";
 
 const router = express.Router();
 const deleteUserController = new DeleteUsuarios();
 const insertUserController = new InsertUsuario();
 const selectGradeAtuacao = new GetGradeAtuacao();
-const insertUpdateAnalistaController = new InsertOrUpdateAtribuicao();
+const insertUpdateAnalistaEditorController = new InsertOrUpdateAtribuicao();
+const insertUpdateAnalistaRevisorController = new InsertOrUpdateValidacao();
 const getStatistcs = new GetStatistcs();
 const getAreaStatistcs = new GetStatistcs();
 const getStatusEditorStatistcs = new GetStatistcs();
@@ -21,8 +23,12 @@ router.post("/usuarios", insertUserController.insertUsuario);
 router.delete("/usuarios", deleteUserController.deleteUsuario),
   router.get("/Gradeatuacao", selectGradeAtuacao.getGradeAtuacao);
 router.post(
-  "/Gradeatuacao",
-  insertUpdateAnalistaController.insertOrUpdateAtribuicao
+  "/InsertAtribuicaoEditor",
+  insertUpdateAnalistaEditorController.insertOrUpdateAtribuicao
+);
+router.post(
+  "/InsertValidacaoRevisor",
+  insertUpdateAnalistaRevisorController.insertOrUpdateValidacao
 );
 router.get("/statistcs", getStatistcs.getStatistics);
 router.get("/tbaoistatistcs", getAreaStatistcs.getAreaStatistics);
